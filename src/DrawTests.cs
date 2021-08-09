ce3f13a70f2f7beaebd72a5ca100c0ae7dcb79ca
Ian Keough <ian@hypar.io>
2021-08-09 16:19:33 -0700

    Add some documentation.

--- src/DrawTests.cs
+++ src/DrawTests.cs
@@ -1,6 +1,5 @@
 using Elements;
 using Elements.Geometry;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -25,21 +24,36 @@ namespace DrawTests
 
             foreach (var beamGroup in structure)
             {
+                // We make arrows and texts in groups of beams because otherwise
+                // we'd run out of space in our label texture map. Splitting up
+                // the text in this way is still no guarantee that you won't 
+                // overrun the atlas, so you'll have to adjust for your case.
                 var texts = new List<(Vector3 location, Vector3 direction, string text, Color? color)>();
                 var arrows = new List<(Vector3, Vector3, double, Color?)>();
                 foreach (var beam in beamGroup)
                 {
+                    // Get some information about the profiles.
+                    // This will be used to calculate the width and the
+                    // depth of the beam, to position the text tag floating
+                    // next to the beam.
                     var profileBounds = new BBox3(((Beam)beam.BaseDefinition).Profile);
                     var height = profileBounds.Max.Y - profileBounds.Min.Y;
                     var width = profileBounds.Max.X - profileBounds.Min.X;
 
+                    // The location of the text is a combination of the 
+                    // base definition's curve center, and the element
+                    // instance's transform.
                     var t = ((Beam)beam.BaseDefinition).Curve.TransformAt(0.5);
                     t.Concatenate(beam.Transform);
                     var l = ((Beam)beam.BaseDefinition).Curve.Length();
                     var dir = t.XAxis;
-                    var ao = t.Origin + new Vector3(0, 0, 1.0);
+
+                    // Create model text data.
                     var to = t.Origin + t.XAxis * width / 2;
                     texts.Add((to, dir, $"{l.ToString("0.##m")}", Colors.Darkgray));
+
+                    // Create model arrow data.
+                    var ao = t.Origin + new Vector3(0, 0, 1.0);
                     arrows.Add((ao, down, 1.0 - height / 2, Colors.Green));
                 }
                 var modelText = new ModelText(texts, FontSize.PT36, input.TextScale);
